cc5d0cdd129041eacc616567bf8acb2da1b38a56
debbyyeh <app2984@gmail.com>
2021-07-06 23:48:53 +0800

    first commit

---
+++ app/assets/script/all.js
@@ -0,0 +1,7 @@
+https://github.com/debbyyeh/gulp.git
+
+git init # 若已經初始化過就可以不用輸入
+git add.
+git commit - m 'first commit'
+git remote add origin https://github.com/debbyyeh/gulp.git
+git push - u origin master // 僅限第一次輸入，往後只需要輸入 git push
